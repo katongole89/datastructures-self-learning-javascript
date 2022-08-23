ebd512308a6b7b082383782b9f789aa01f92c79b
katongole89 <katongolerichard089@gmail.com>
2022-08-23 15:17:36 +0300

    looping thru a string

--- Stacks/index.js
+++ Stacks/index.js
@@ -1,1 +1,27 @@
 //STACK BASICS
+
+//DEFINITIONS
+// Last in first out
+
+// Example
+// Stack of books
+// browser back button
+//CODE
+
+// Functions 
+//push - placing data on a stack
+// pop -removing top element of stack
+//peek - displaying top element of a stack
+//length-determiining how many elements are in a stack
+
+// array obj alreay has all the function
+
+
+let letters = []
+
+let word = 'racecar'
+let rword = ""
+
+for (const s of str) {
+  console.log(s)
+}
