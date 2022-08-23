f94ad8a8731731064d3b298199356f5231ec6e85
katongole89 <katongolerichard089@gmail.com>
2022-08-23 15:39:38 +0300

    how to check if a string is a parlindrome

--- Stacks/index.js
+++ Stacks/index.js
@@ -26,3 +26,23 @@ for (const s of word) {
   console.log(s)
   letters.push(s)
 }
+
+// checking if a word is a parlindrome
+// for (const s of word) {
+//   console.log(s)
+//   letters.push(s)
+// }
+
+// letters.forEach((letter)=>{
+//     rword += letter
+// })
+
+
+console.log('current letters variable after pushing', letters)
+
+//popping
+letters.pop()
+console.log('current letters variable after popping', letters)
+
+
+
